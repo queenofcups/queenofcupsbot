b02429774dfec856e88a51455337bde542bd710d
queenofcups <31224508+queenofcups@users.noreply.github.com>
2017-08-22 11:11:31 +0300

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-
-var config = {
-  consumer_key:         'Duk6iV29sagRNNfuRNDO7qjtT',
-  consumer_secret:      'hDtbyNxQDk5rc8w7agpHHbrhrcKMhtZ1TN17mmLjRRY3tqEVmw',
-  access_token:         '898557456648982528-5hgabrRhnM77y3YdCqw8SAtSO1gXuUi',
-  access_token_secret:  'a3Mai2pTqsnl7g5rbfBG9iIiyPKBxkwx3omu2dWFPsepN',
-}
-module.exports = config;
